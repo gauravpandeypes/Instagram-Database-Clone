1a8d3b71b36d6a47c1a7e1b7be1090d0dc5c8d25
GauravPandey892 <892dgauravpandey@gmail.com>
2020-06-14 10:53:40 +0530

    first commit

---
+++ code.sql
@@ -0,0 +1,139 @@
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+)
+
+INSERT INTO users (username) VALUES
+('Gaurav'),
+('PES University');
+
+SELECT * FROM users;
+
+CREATE TABLE photos (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+INSERT INTO photos (image_url, user_id) VALUES
+('http://digitallearning.eletsonline.com/wp-content/uploads/2016/05/pes-university.jpg',2);
+
+SELECT * FROM photos;
+
+CREATE TABLE comments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    photo_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE likes (
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id)
+);
+
+CREATE TABLE follows (
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
+
+CREATE TABLE unfollows (
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
+
+CREATE TABLE tags (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(255) UNIQUE,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags (
+    photo_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+);
+
+-- Trigger
+DELIMITER $$
+
+CREATE TRIGGER create_unfollow
+    AFTER DELETE ON follows FOR EACH ROW
+BEGIN
+    INSERT INTO unfollows
+    SET follower_id = OLD.follower_id,
+        followee_id = OLD.followee_id;
+END$$
+
+DELIMITER ;
+
+
+
+-- Query
+
+1)
+SELECT
+  (SELECT COUNT(*) FROM photos) /
+  (SELECT COUNT(*) FROM users)  AS average_photo_per_user;
+
+2)
+SELECT
+  tags.tag_name,
+  COUNT(*) as total
+FROM photo_tags
+JOIN tags
+  ON photo_tags.tag_id = tags.id
+GROUP BY tags.id
+ORDER BY total DESC
+LIMIT 5;
+
+3)
+SELECT
+  username,
+  photos.id,
+  photos.image_url,
+  COUNT(*) AS total
+FROM photos
+INNER JOIN likes
+  ON likes.photo_id = photos.id
+INNER JOIN users
+  ON photos.user_id = users.id
+GROUP BY photos.id
+ORDER BY total DESC
+
+4)
+SELECT
+  username,
+  COUNT(*) AS num_likes
+FROM users
+INNER JOIN likes
+  ON users.id = likes.user_id
+GROUP BY likes.user_id
+HAVING num_likes = (SELECT COUNT(*) FROM photos);
+
+5)
+SELECT username
+FROM users
+LEFT JOIN photos
+  ON users.id = photos.user_id
+WHERE photos.id IS NULL;
